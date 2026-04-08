f1af7250b7665713cbc9aab2eb2ddbc1d631d100
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-04-08 09:28:39 -0300

    created jwt tokens

---
+++ Backend/src/utils/jwt/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
+
+function generateAccessToken(user) {
+    return jwt.sign({userId: user.id}, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: '10m'
+    });
+}
+
+function generateRefreshToken() {
+    const token = crypto.randomBytes(16).toString('base64url');
+    return token;
+}
+
+function generateTokens(user) {
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken();
+    return {accessToken, refreshToken}
+}
+
+module.exports = {
+    generateAccessToken,
+    generateRefreshToken,
+    generateTokens
+}
