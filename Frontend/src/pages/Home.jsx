67c2ef6bd0fc6a582033d1dcee41632c14ff62e7
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-04-16 09:46:40 -0300

    added navigation to Home page on sucessful user login

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import LoginForm from "../features/auth/LoginForm/LoginForm";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 
 function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let navigate = useNavigate();
 
   useEffect(() => {
     if (isLoggedIn == true) {
+      navigate("/messages");
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, navigate]);
 
   return (
-    <div class="home">
+    <div className="home">
       <LoginForm setIsLoggedIn={setIsLoggedIn} />
     </div>
   );
