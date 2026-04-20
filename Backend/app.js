e0043ad9ca206e85b0317f28ca4573bc2fddf4d1
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-04-20 09:06:06 -0300

    added proper response header configuration

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/assets', express.static(path.join(__dirname, "assets")))
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*" );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
 app.use('/', index)
 
 module.exports = app;
