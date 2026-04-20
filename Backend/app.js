6a5f68d80e9958f24eb19e95ff1d058259b38321
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-04-20 10:11:04 -0300

    removed unneded code in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,20 +11,6 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/assets', express.static(path.join(__dirname, "assets")))
 
-/*
-}*/
-
-/*app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173" );
-    res.setHeader("Access-Control-Allow-Credentials", "true")
-    res.setHeader("Vary", "Origin")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})*/
-
 app.use('/', index)
 
 module.exports = app;
