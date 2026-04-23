c1a746253e64d5c5bb6a2b745ac53e407ea45d07
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-04-23 09:42:54 -0300

    wrote last tests for UserCard

--- Frontend/src/components/users/UserCard/UserCard.test.jsx
+++ Frontend/src/components/users/UserCard/UserCard.test.jsx
@@ -1,9 +1,21 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import UserCard from "./UserCard";
 
-const user = { username: "pete", name: "pete the meek", portrait: "pete.jpg" };
-
+const user = {
+  id: "sdasdsa",
+  username: "pete",
+  name: "pete the meek",
+  portrait: "pete.jpg",
+};
+const talkingWith = {
+  id: "sdassdsssdsa",
+  username: "peter",
+  name: "peter the meeker",
+  portrait: "peter.jpg",
+};
+const handleTalkingWith = vi.fn();
 describe("UserCard", () => {
   it("should render the main container", () => {
     render(<UserCard user={user} />);
@@ -19,5 +31,34 @@ describe("UserCard", () => {
     const image = screen.getByRole("img");
 
     expect(image).toBeInTheDocument();
+  });
+
+  it("should not call the function", async () => {
+    render(
+      <UserCard
+        user={user}
+        talkingWith={talkingWith}
+        handleTalkingWith={handleTalkingWith}
+      />,
+    );
+
+    expect(handleTalkingWith).not.toHaveBeenCalled();
+  });
+
+  it("should call the function", async () => {
+    const user = userEvent.setup();
+    render(
+      <UserCard
+        user={user}
+        talkingWith={talkingWith}
+        handleTalkingWith={handleTalkingWith}
+      />,
+    );
+
+    const container = screen.getByTestId("container");
+
+    await user.click(container);
+
+    expect(handleTalkingWith).toHaveBeenCalled();
   });
 });
