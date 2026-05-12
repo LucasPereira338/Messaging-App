c6ba453793b8158ec7ea5753355598fdca70b248
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-05-12 14:49:12 -0300

    wrote fetch functions for groups

---
+++ Frontend/src/services/groupServices.js
@@ -0,0 +1,88 @@
+export async function fetchGroupMessages(data) {
+    const backend = import.meta.env.VITE_BACKEND
+    
+    const url = backend + 'groups/' + data.id + 'messages';
+
+    const token = localStorage.getItem('token')
+    
+    const response = await fetch(url, {
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token
+        }
+    })
+    return response.json()
+}
+
+export async function fetchGroupUsers(data) {
+    const backend = import.meta.env.VITE_BACKEND
+    
+    const url = backend + 'groups/' + data.id + 'users';
+
+    const token = localStorage.getItem('token')
+    
+    const response = await fetch(url, {
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token
+        }
+    })
+    return response.json()
+}
+
+export async function fetchGroup(data) {
+    const backend = import.meta.env.VITE_BACKEND
+    
+    const url = backend + 'groups/' + data.id 
+
+    const token = localStorage.getItem('token')
+    
+    const response = await fetch(url, {
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token
+        }
+    })
+    return response.json()
+}
+
+export async function fetchUserGroups(data) {
+    const backend = import.meta.env.VITE_BACKEND
+    
+    const url = backend + 'users/' + data.id + '/groups'
+
+    const token = localStorage.getItem('token')
+    
+    const response = await fetch(url, {
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token
+        }
+    })
+    return response.json()
+}
+
+export async function addGroupMembers(data) {
+    const backend = import.meta.env.VITE_BACKEND
+    
+    const url = backend + 'groups/' + data.id 
+
+    const token = localStorage.getItem('token')
+    
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token
+        },
+        body: JSON.stringify(data)
+    })
+    return response.json()
+}
+
+export async function createGroup(data) {
+    const backend = import.meta.env.VITE_BACKEND
+    
+    const url = backend + 'groups/' 
+
+    const token = localStorage.getItem('token')
+    
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token
+        },
+        body: JSON.stringify(data)
+    })
+    return response.json()
+}
+
