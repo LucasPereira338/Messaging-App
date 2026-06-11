e5d4dab9b6658895ab8c4504687e2aa8e8263385
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-06-11 09:36:36 -0300

    ChatMessages: Made the user's scrollbar start at the bottom instead of the top, meaning the newest messages show up first

--- Frontend/src/components/messages/ChatMessages/ChatMessages.jsx
+++ Frontend/src/components/messages/ChatMessages/ChatMessages.jsx
@@ -1,9 +1,23 @@
 import * as styles from "./ChatMessages.module.css";
+import { useRef, useEffect } from "react";
 import Message from "../Message/Message";
 
 function ChatMessages({ messages = [], setMsgToDel }) {
+  const msgsRef = useRef(null);
+
+  useEffect(() => {
+    const msgs = msgsRef.current;
+    if (msgs) {
+      msgs.scrollTop = msgs.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <section className={styles.chatMessages} data-testid="ChatMessages">
+    <section
+      className={styles.chatMessages}
+      data-testid="ChatMessages"
+      ref={msgsRef}
+    >
       {messages
         ? messages.map((item) => {
             return (
