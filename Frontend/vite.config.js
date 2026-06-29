02bc2aab2692ef4cabc0aa9b86fbf854af4f1d4c
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-06-29 10:48:54 -0300

    added additional configurations to vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    open: true,
+    
+    hmr: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
