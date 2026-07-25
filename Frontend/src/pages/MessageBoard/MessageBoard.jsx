7437532cc680d4e8cd78139152cfe8dfe49a4117
LucasPereira338 <lucasgabriel.floresta@gmail.com>
2026-07-25 13:35:39 -0300

    made the navigate function in the MessageBoard component wait until the asynchronous backend call is finished before redirecting the user to the Home page

--- Frontend/src/pages/MessageBoard/MessageBoard.jsx
+++ Frontend/src/pages/MessageBoard/MessageBoard.jsx
@@ -43,9 +43,9 @@ function MessageBoard() {
     } else {
       const logUserOut = async () => {
         await userLogout();
+        navigate("/");
       };
       logUserOut();
-      navigate("/");
     }
   };
 
